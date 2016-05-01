8dba0d78a7a7f99d0cb0ee7507b2ae7ccd6f6541
Nettle <ctmann@gmail.com>
2016-05-01 14:59:34 -0400

    fixing prettynums for negative values

--- R/Number_Formats.R
+++ R/Number_Formats.R
@@ -5,42 +5,25 @@
 #' Numbers that have 12 digits are abreviated "T" for trillion
 #' Numbers that have 9 digits are abreviated "B" for billion
 #' Numbers that have 6 digits are abreviated "M" for Million
-#' Commas are inserted
+#' Commas are inserted. Some warning messages remain. Not sure why.
 #'
 #'
 #' @param num A big or small number
 #' @usage nettle_prettyNum(c(1000, -1000, 1000000, -1000000, 1000000000, -1000000000, 1000000000000, -1000000000000))
 #'
 #' @export
-nettle_big.numbers <- function(num) {
-  t.index = num >= 1e12
-  b.index = num >= 1e9 & num < 1e12
-  m.index = num >= 1e5 & num < 1e9
-  k.index = num >= 1e3 & num < 1e5
-  output = formatC(num, format = "d", big.mark = ",")
-  
-  output[t.index] = paste(formatC(num[t.index] / 1e12, digits = 1,format = "f"), "T")
-  output[b.index] = paste(formatC(num[b.index] / 1e9, digits = 1, format = "f"), "B")
-  output[m.index] = paste(formatC(num[m.index] / 1e6, digits = 1, format = "f"), "M")
-  output[k.index] = paste(formatC(num[k.index] / 1e3, digits = 1, format = "f"), "K")
-  return(output)
-}
-nettle_small.numbers <- function(num) {
-  t.index = num <= -1e12
-  b.index = num <= -1e9 & num > -1e12
-  m.index = num <= -1e5 & num > -1e9
-  k.index = num <= -1e3 & num > -1e5
-  output = formatC(num, format = "d", big.mark = ",")
-  
-  output[t.index] = paste(formatC(num[t.index] / 1e12, digits = 1,format = "f"), "T")
-  output[b.index] = paste(formatC(num[b.index] / 1e9, digits = 1, format = "f"), "B")
-  output[m.index] = paste(formatC(num[m.index] / 1e6, digits = 1, format = "f"), "M")
-  output[k.index] = paste(formatC(num[k.index] / 1e3, digits = 1, format = "f"), "K")
-  return(output)
-}
 nettle_prettyNum <- function(num){
-  ifelse(num > 0, 
-         printed.number <- nettle_big.numbers(num),
-         printed.number <- nettle_small.numbers(num))
-  return(printed.number)
+  t.index <- (num >= 1e12) | (num <= -1e12)
+  b.index <- (num >= 1e9 & num < 1e12)| (num <= -1e9 & num > -1e12)
+  m.index <- (num >= 1e5 & num < 1e9) | (num <= -1e5 & num > -1e9)
+  k.index <- (num >= 1e3 & num < 1e5) |(num <= -1e3 & num > -1e5)
+  output = formatC(num, format = "d", big.mark = ",")
+  
+  output[t.index] = paste(formatC(num[t.index] / 1e12, digits = 1,format = "f"), "T")
+  output[b.index] = paste(formatC(num[b.index] / 1e9, digits = 1, format = "f"), "B")
+  output[m.index] = paste(formatC(num[m.index] / 1e6, digits = 1, format = "f"), "M")
+  output[k.index] = paste(formatC(num[k.index] / 1e3, digits = 1, format = "f"), "K")
+  return(output)
 }
+  
+
